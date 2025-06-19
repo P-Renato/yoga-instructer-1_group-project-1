7a5d9a765e62d98660e993288237e3a41b112065
Renato <paulo.borupdebakker@dci-student.org>
2025-06-19 22:22:10 +0200

    first page updates

---
+++ src/Header.jsx
@@ -0,0 +1,24 @@
+
+
+import "./App.css";
+import "./Header.css";
+
+
+export default function Header () {
+    return (
+
+    
+    <>
+        <header className="header">
+            <nav className="navBar">
+                <a href="">About me</a>
+                <a href="">Schedule</a>
+                <a href="">Event</a>
+                <a href="">Blog</a>
+            </nav>
+            <h1>Carolina Rose</h1>
+            <button>Contact</button>
+        </header>
+    </>
+    )
+}
