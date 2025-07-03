2db60d83e96761583c286ea9eef2ff52cb07083a
Renato <paulo.borupdebakker@dci-student.org>
2025-07-03 16:57:36 +0200

    update 3.07 renato

--- FrontEnd/src/views/Blog.jsx
+++ FrontEnd/src/views/Blog.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import "../App.css";
-import "./styles/Blog.css";
+import "./Blog.css";
 import { useEffect, useState } from "react";
 
 export default function Blog () {
