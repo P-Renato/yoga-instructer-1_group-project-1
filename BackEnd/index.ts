d09d55ffa068e2a9bf6f4d05efdacb50d79c470c
Renato <paulo.borupdebakker@dci-student.org>
2025-10-23 19:19:11 +0200

    Fix CORS origin - remove trailing slash

--- BackEnd/index.ts
+++ BackEnd/index.ts
@@ -74,9 +74,8 @@ const app = express();
 // middleware
 app.use(cors({
   origin: [
-    'https://yoga-instructer-1-group-project-1.onrender.com/', 
-    'http://localhost:5173',                   
-    'http://localhost:3000'                    
+    'https://yoga-instructer-1-group-project-1.onrender.com', 
+    'http://localhost:5173'                    
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
